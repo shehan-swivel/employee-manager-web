682d868a860db833a3b2376b37db6564e8d1f806
Shehan Rangana <shehanr@swivelgroup.com.au>
2023-04-20 20:10:34 +0530

    Navbar title linked to home page.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Employee Manager
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            <Link shallow href="/employee/list" style={{ textDecoration: "none", color: "inherit" }}>
+              Employee Manager
+            </Link>
           </Typography>
         </Toolbar>
       </AppBar>
